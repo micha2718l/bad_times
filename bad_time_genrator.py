f76a0fb1e2a342d6318eac86395b0ffdf6853fae
Michael <micha2718l@gmail.com>
2020-06-16 21:22:50 -0500

    adding flask

--- bad_time_genrator.py
+++ bad_time_genrator.py
@@ -1,5 +1,10 @@
 import random
 
+import Flask from flask
+
+
+app = Flask(__name__)
+
 with open('negative_words.txt') as f:
     neg_words = f.readlines()
     neg_words = [n.strip() for n in neg_words]
@@ -8,4 +13,9 @@ with open('animals.txt') as f:
     animals = f.readlines()
     animals = [a.strip() for a in animals]
 
-print(random.choice(neg_words) + ' ' + random.choice(animals))
+def bad_time():
+    return random.choice(neg_words) + ' ' + random.choice(animals))
+
+@app.route('/')
+def hello_world():
+    return bad_time()
